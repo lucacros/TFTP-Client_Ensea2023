0e432b5d29c0afdb768e47b0d169287058729452
Manon GIANINO <manon.gianino@gmail.com>
2023-12-13 14:56:33 +0100

    Q1_bis

---
+++ puttftp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+//
+
+
+
+int main (int argc, char*argv[]){
+    char host[20], file[20];
+    strcpy (host,argv[1]); //copy the adress in the host
+    strcpy (file,argv[2]); //copy the file's name in file
+    
+    printf("Host: %s\n",host);
+    printf("File: %s\n",file);
+
+}
